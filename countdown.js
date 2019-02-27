c9c62f1d27747b0bafec28eceb6928c7a9ad411f
(Jeremy Peyrat) prstoad@gmail.com <prstoad@gmail.com>
2019-02-27 16:01:23 +0100

    coutdown

---
+++ countdown.js
@@ -0,0 +1,45 @@
+const $div = document.getElementById("countdown");
+
+// let max = 3;
+// let timeMax = setInterval(countD, 1000);
+
+// function countD(){
+// $div.textContent = max;
+//    max--;
+//    if(max < 0){
+//       clearInterval(timeMax);
+//    }
+// }
+
+// ac le setTimeOut
+// let start = 3;
+
+// for(let i = start; i >= 0; i--){
+//   setTimeout(function(){
+//     $div.textContent = i;
+//   }, (start - i +1) *1000);
+// }
+
+function CountDown(){
+
+let count = 0;
+
+  function start(){
+    for(let i = count; i < 1000; i++){
+      setTimeout(function(){
+        $div.textContent = i;
+      }, (count+i) * 1000);
+    }
+  }
+  function stop(){
+    $div.addEventListener("click", function(){
+
+    });
+  }
+  function reset(){
+
+  }
+  start();
+}
+
+CountDown();
